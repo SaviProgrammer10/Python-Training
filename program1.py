6759e3fb405420f448456ffb40b97aafcb853071
SaviProgrammer10 <savita.dhingra@codingal.com>
2026-07-24 21:44:53 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Savita")
+
+# Printing a fun fact about myself
+# print("I love playing Badminton.")
+print("I love playing Badminton.")
+
+# Performing and printing a simple calculation
+print("The result of 7 + 5 is:", 7 + 5)
