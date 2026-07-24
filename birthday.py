b00bf604dd0812b68e34c77f47496477bbfbb272
SaviProgrammer10 <savita.dhingra@codingal.com>
2026-07-24 22:14:24 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,15 @@
+# Store 5 birthdays in variables
+
+birthday1 = "12-01-2005"
+birthday2 = "25-03-2006"
+birthday3 = "18-07-2004"
+birthday4 = "09-11-2005"
+birthday5 = "30-12-2006"
+
+# Print the birthdays
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
